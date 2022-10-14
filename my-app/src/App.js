a2bc35320812e9b1599663e54176cd5e73c8a12d
Esdras <esdras12thiago@gmail.com>
2022-10-14 17:31:03 -0300

    Adicionando musiquinha ao site

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Forms from './pages/Forms';
 import Calculator from './pages/Calculator'
 import NotLogged from './pages/NotLogged';
 import NotFound from './pages/NotFound';
 import './App.css';
-import { Switch } from 'react-router-dom';
+import genshinOst from './assets/musics/genshin-ost.mp3'
 
 
 class App extends React.Component {
+  autoDecrement = (event) => {
+    event.target.volume = 0.05
+  }
+
   render() {
 
     return (
       <div className='all'>
+      <audio loop src={ genshinOst } autoPlay onPlay={ this.autoDecrement } />
       <BrowserRouter>
       <Switch>
       <Route exact path="/" component={ Login }/>
